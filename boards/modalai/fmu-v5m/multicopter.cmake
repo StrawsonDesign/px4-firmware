99b2079a95ccecfc2f1a48e8415d58d98776324c
Travis Bottalico <travis@modalai.com>
2019-09-12 12:46:14 -0700

    Begin of RevB updates
    - change from ICM2048 to ICM20602 on SPI1
    - enable SPI6 in defconfig for BMI088 (not enabled on startup yet)
    - move LED 2 BLUE from PH12 to PA2

--- boards/modalai/fmu-v5m/multicopter.cmake
+++ boards/modalai/fmu-v5m/multicopter.cmake
@@ -29,10 +29,9 @@ px4_add_board(
 		imu/adis16448
 		imu/adis16497
 		#imu # all available imu drivers
-# TBD		imu/bmi088 - needs bus selection
+		imu/bmi088
 # TBD		imu/ism330dlc - needs bus selection
 		imu/mpu6000
-		imu/icm20648
 		irlock
 		lights/blinkm
 		lights/oreoled
